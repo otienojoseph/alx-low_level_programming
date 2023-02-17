236fb6b70f80217ac2ef09c22c39e4737b9bb5c1
otienojoseph <otienoj.cruz@gmail.com>
2023-02-18 02:10:01 +0300

    Add 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers 0 - 9 using putchar
+ * Return: return (0) success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -4,3 +4,4 @@
 - 3-print_alphabets: Print alphabets in lower and upper case
 - 4-print_alphabet: Print alphabets in lowercase except 'e' and 'q'
 - 5-print_numbers: Print numbers from 0 - 9
+- 6-print_numberz: Print numbers using putchar
