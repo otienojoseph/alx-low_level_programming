ab21302235da067167a6f03d1b107f74bfea47fc
otienojoseph <otienoj.cruz@gmail.com>
2023-02-18 02:57:46 +0300

    Fix: Add correct code to print single digit integers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,20 +6,12 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 1; j < 10; j++)
-		{
-			putchar(i + '0');
-			putchar(j + '0');
-
-			if (i < 9 || j < 9)
-			{
-				putchar(',');
-			}
-		}
+		putchar(i + '0');
+		putchar(',');
 	}
 	putchar('\n');
 
