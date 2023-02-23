e1f552d571f8a97b03090737f87951cf48868b38
otienojoseph <otienoj.cruz@gmail.com>
2023-02-23 05:03:16 +0300

    Fix: Add new checks accounting for int_min and int_max

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,6 +12,21 @@ int print_last_digit(int n)
 	int last_digit;
 
 	last_digit = abs(n) % 10;
+
+	if (n == INT_MIN)
+	{
+		last_digit = 8;
+	}
+	else if (n == INT_MAX)
+	{
+		last_digit = 7;
+	}
+
+	if (n < 0)
+	{
+		last_digit *= -1;
+	}
+
 	_putchar(last_digit + '0');
 
 	return (last_digit);
