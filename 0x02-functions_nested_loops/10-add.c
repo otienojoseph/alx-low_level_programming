d3ba79bf9f0a8354aaf903809a5fe72cf4dd4685
otienojoseph <otienoj.cruz@gmail.com>
2023-02-24 20:20:56 +0300

    Fix: fix error on putchar

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Add two integers and return result
+ * @a: param
+ * @b: param
+ * Return: return result
+ */
+
+int add(int arg1, int arg2)
+{
+	int sum;
+
+	sum = (arg1 + arg2);
+	return (sum);
+}
