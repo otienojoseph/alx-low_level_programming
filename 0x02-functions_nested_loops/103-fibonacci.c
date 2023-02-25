e9398b6747f9091c5893ddf56effc5f5bb45e5b3
otienojoseph <otienoj.cruz@gmail.com>
2023-02-25 15:38:02 +0300

    Fix: Add correct sumation of items

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,20 +7,22 @@
 
 int main(void)
 {
-	int sum, a, b;
+	int sum, a, b, c;
 
 	sum = 0;
 
 	a = 1, b = 2;
-	
-	while (a < 4000000)
+
+	while (a <= 4000000)
 	{
+		c = a + b;
+
 		if (a % 2 == 0)
 		{
 			sum += a;
 		}
 		a = b;
-		b = a + b;
+		b = c;
 	}
 
 	printf("%i", sum);
