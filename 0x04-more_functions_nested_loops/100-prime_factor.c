f72d5a12d75e1db994bb71aa02e90c6e06164207
otienojoseph <otienoj.cruz@gmail.com>
2023-02-26 21:51:23 +0300

    Fix: Make it betty complalint

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
-    long n; 
-    int i, largest_factor;
+/**
+ * main - Find largest prime factor
+ * Return: Always 0
+ */
+int main(void)
+{
+	long n;
+	int i, largest_factor;
 
 	largest_factor = 1;
 	n = 612852475143;
 
-    /* Check for 2 as a factor repeatedly */
-    while (n % 2 == 0) {
-        largest_factor = 2;
-        n = n / 2;
-    }
+	/* Check for 2 as a factor repeatedly */
+	while (n % 2 == 0)
+	{
+		largest_factor = 2;
+		n = n / 2;
+	}
 
-    /* Check for odd factors up to the square root of n */
-    for (i = 3; i <= sqrt(n); i += 2) {
-        while (n % i == 0) {
-            largest_factor = i;
-            n = n / i;
-        }
-    }
+	/* Check for odd factors up to the square root of n */
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			largest_factor = i;
+			n = n / i;
+		}
+	}
 
     /* If n is still greater than 2, it must be a prime factor */
-    if (n > 2) {
-        largest_factor = n;
-    }
+	if (n > 2)
+	{
+		largest_factor = n;
+	}
 
-    printf("%d\n", largest_factor);
+	printf("%d\n", largest_factor);
 
-    return 0;
+	return (0);
 }
-
