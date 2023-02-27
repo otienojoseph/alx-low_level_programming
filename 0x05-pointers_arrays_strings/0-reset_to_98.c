44da568365e349fc2afe56e0c488b129c22b58ef
otienojoseph <otienoj.cruz@gmail.com>
2023-02-28 00:47:33 +0300

    Add 0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Update value at pointer
+ * @n: param
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# ARRAYS AND POINTERS
+
+- 0-reset_to_98: Takes a pointer and updates it's value
