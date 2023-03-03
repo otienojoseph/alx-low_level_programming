0f59f628e4cccf98a09f06591eafcffa0b4f689f
otienojoseph <otienoj.cruz@gmail.com>
2023-03-03 19:10:28 +0300

    Add 1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strncat - Concatenate two strings and don't need
+* to be null terminated
+* @dest: param pointer
+* @src: param pointer
+* n: int param
+* Return: char pointer
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+
+	n = sizeof(dest);
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		 dest[n + i] = src[i];
+	}
+	dest[n + i] = '\0';
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 # Pointers Arrays and Strings
 
 - 0-strcat.c: Appends 'src' string to 'dest' string overwriting the terminating null byte (\0)
+- 1-strncat: Concatenate two strings
