119d1d8cbb53a826c38d8d1937272c4b4a0328fe
otienojoseph <otienoj.cruz@gmail.com>
2023-03-08 22:13:01 +0300

    Add 0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Print string
+ * @s: pointer param
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+# Recursion
+
+- 0-puts_recursion: Print string followed by a newline
