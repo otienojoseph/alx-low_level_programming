71e25057f39b22399215b33a7344f40685c3df3b
otienojoseph <otienoj.cruz@gmail.com>
2023-03-28 01:14:41 +0000

    fix: Add betty compliance

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 /**
  * main - print name followed by a newline
+ * @argc: param
+ * @argv: param
  * Return: success
  */
 
@@ -9,5 +11,5 @@ int main(int argc, char *argv[])
 {
 	if (argc > 0)
 		printf("%s\n", argv[0]);
-	return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
