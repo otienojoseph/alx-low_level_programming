16c810802e1eaa436e4dc84014f61e78fe2d4649
OptimusJc <otienoj.cruz@outlook.com>
2023-04-04 22:19:55 +0000

    Add 1-listint_len

--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -1,3 +1,4 @@
 # More Linked Lists
 
 - 0-print_listint: Print all elements of list
+- 1-listint_len: Returns the number of elements in a linked list
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -14,5 +16,6 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif
