e352028522ca266b6c825443c13e8ed8755dafa4
otienojoseph <otienoj.cruz@gmail.com>
2023-06-13 18:26:21 +0000

    Fix: Add code to subtract 1 from argv array

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,6 +11,14 @@ int main(int argc, char **argv)
 {
 	(void) **argv;
 
-	printf("%d\n", argc);
+	if (argc > 1)
+	{
+		printf("%d\n", argc - 1);
+	}
+	else
+	{
+		printf("%d\n", 0);
+	}
+
 	return (0);
 }
