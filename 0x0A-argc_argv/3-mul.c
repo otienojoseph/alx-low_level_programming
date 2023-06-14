9cc0aea49b58db9ff773036d899ecb470a4245b6
otienojoseph <otienoj.cruz@gmail.com>
2023-06-14 10:43:47 +0000

    Add function that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiply two numbers and return result
+ * @argc: argument count
+ * @argv: argument array
+ * Return: On success 0
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -3,3 +3,4 @@
 -0-whatssmyname - Print name of function
 -1-arg.c - Print number of arguments
 2-args.c - Print all args per line
+3-mul.c - Multiplies two numbers passed as args
