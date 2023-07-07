b08b4f87a6500aea9dbab4f5be3e5774803345c8
otienojoseph <otienoj.cruz@gmail.com>
2023-07-07 21:37:37 +0000

    Add 1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - Print the number of args passed
+  * @argc: Argument count
+  * @argv: Argument vector
+  * Return: Always 0
+  */
+
+int main(int argc, char *argv[])
+{
+	(void) *argv;
+	printf("%d\n", argc);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,4 @@
 # Argc and Argv
 
 - 0-whatismyname: Print name of the function
+- 1-args: Print number of args passed
