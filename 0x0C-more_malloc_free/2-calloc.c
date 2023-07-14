1567fe3e629ae8c2d923f1a58a24f4671e181a26
otienojoseph <otienoj.cruz@gmail.com>
2023-07-14 17:37:07 +0000

    Change pointer to char

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,8 +10,8 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	u_int8_t *ptr;
-    unsigned int i;
+	char *ptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -21,10 +21,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-    for (i = 0; i < nmemb; i++)
-    {
-        ptr[i] = 0;
-    }
+	for (i = 0; i < nmemb; i++)
+	{
+		ptr[i] = 0;
+	}
 
 	return (ptr);
 }
