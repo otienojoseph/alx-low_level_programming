4a47de3a7c5eba65cfb595454ea6fb7a722f2004
otienojoseph <otienoj.cruz@gmail.com>
2023-07-17 17:46:50 +0000

    Add 2-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print name of file
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -1,3 +1,4 @@
 # Macros
 - 0-object_like_macro: Define macro named SIZE
 - 1-pi: Define a PI macro
+- 2-main: Print name of file
