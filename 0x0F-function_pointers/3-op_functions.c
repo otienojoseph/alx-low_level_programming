c674f0a49380c38e5fba41afe71db26efc8fe0cf
otienojoseph <otienoj.cruz@gmail.com>
2023-07-19 22:34:14 +0000

    Add 3-calc header file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Return sum of two ints
+ * @a: param
+ * @b: param
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Return difference of two ints
+ * @a: param
+ * @b: param
+ * Return: difference of two ints
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Return product of two ints
+ * @a: param
+ * @b: param
+ * Return: product of two ints
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Return result of division of two ints
+ * @a: param
+ * @b: param
+ * Return: divission of two ints
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Return remainder of division of two ints
+ * @a: param
+ * @b: param
+ * Return: remainder of division
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -3,3 +3,4 @@
 - 0-print_name: Print name
 - 1-array_iterator: Execute function given as parameter on each element of array
 - 2-int_index: Write a function that searches for an integer
+- 3-calc: Perform simple mathematical operations
