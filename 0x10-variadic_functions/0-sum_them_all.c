5174fa15243243e18302c0ecf464b175a952bca4
otienojoseph <otienoj.cruz@gmail.com>
2023-07-20 12:57:48 +0000

    Add check for n == 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,6 +16,9 @@ int sum_them_all(const unsigned int n, ...)
 	result = 0;
 	va_start(args, n);
 
+    if (n == 0)
+        return (0);
+
 	for (i = 0; i < n; i++)
 	{
 		result += va_arg(args, int);
