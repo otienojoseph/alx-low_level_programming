06f071893a5cb1b1268636a7a9c5c83024cf7888
OptimusJc <optimus.prime.jc@gmail.com>
2023-08-01 15:41:03 +0300

    Add check for double pointer null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current, *next;
 
-	current = *head;
-	while (current != NULL)
+	if (head != NULL && *head != NULL)
 	{
-		next = current->next;
+		current = *head;
+		while (current != NULL)
+		{
+			next = current->next;
 
-		free(current);
-		current = next;
+			free(current);
+			current = next;
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
