752e4cdf9696a7f119ff8266b7796d80bd97e6cf
OptimusJc <optimus.prime.jc@gmail.com>
2023-08-01 15:31:32 +0300

    Fix betty issues

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,15 +7,15 @@
  */
 void free_listint2(listint_t **head)
 {
-   listint_t *current, *next;
+	listint_t *current, *next;
 
-   current = *head;
-   while (current != NULL)
-   {
+	current = *head;
+	while (current != NULL)
+	{
 		next = current->next;
 
 		free(current);
 		current = next;
-   }
-   *head = NULL;
+	}
+	*head = NULL;
 }
