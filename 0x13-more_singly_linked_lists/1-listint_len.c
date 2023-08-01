64309bd1b2c496bb0c2db8c8b6b178a100c8d326
OptimusJc <optimus.prime.jc@gmail.com>
2023-08-01 12:21:47 +0300

    Add 1-listint

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - Return number of elements in linked list
+ * @h: pointer to structure
+ * Return: number of elements in linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -1,1 +1,3 @@
+# More Linked List
 - 0-print_listint - Print all elements of listint_t list
+- 1-listint_len - Return number of elements in a linked list
