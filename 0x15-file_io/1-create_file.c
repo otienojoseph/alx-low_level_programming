c9620d8af4895ea18ca2c051386996f5d47b8a26
otienojoseph <otienoj.cruz@gmail.com>
2023-08-22 13:36:50 +0300

    Add 1-create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - Create file
+ * @filename: filename
+ * @text_content: text content
+ * Return: 1 on success -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+	   return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		perror("fails\n");
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			perror("fail\n");
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+
+	return (1);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,3 +1,4 @@
 # File IO
 
 - 0-read_textfile.c - Function that reads a text file and prints it to the POSIX standard output
+- 1-create_file: Create file with read and write permissions for user
