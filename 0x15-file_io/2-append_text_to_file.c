d1709eb7e3859d4a1919b05db7fe3f419c2b8160
otienojoseph <otienoj.cruz@gmail.com>
2023-08-22 15:00:26 +0300

    Add 2-append_text_to_file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - Create file
+ * @filename: filename
+ * @text_content: text content
+ * Return: 1 on success -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, len;
+	ssize_t bytes_written;
+
+	len = 0;
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR| O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	while (text_content && *(text_content + len))
+		len++;
+
+	bytes_written = write(fd, text_content, len);
+	close(fd);
+
+	if (bytes_written < 0)
+		return (-1);
+
+	return (1);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -2,3 +2,4 @@
 
 - 0-read_textfile.c - Function that reads a text file and prints it to the POSIX standard output
 - 1-create_file: Create file with read and write permissions for user
+- 2-append_text_to_file: Appends text at the end of a file
