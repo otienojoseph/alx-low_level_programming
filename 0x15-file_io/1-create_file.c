75c7d6fd57d4418eb73edb4d76e849b069012dd2
otienojoseph <otienoj.cruz@gmail.com>
2023-08-22 13:59:43 +0300

    Fix: Add filemode append

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,9 +12,9 @@ int create_file(const char *filename, char *text_content)
 	ssize_t bytes_written;
 
 	if (filename == NULL)
-	   return (-1);
+		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		perror("fails\n");
