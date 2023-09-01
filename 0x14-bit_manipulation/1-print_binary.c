742097d7d4320539b8d32cfbe5fa5ef3a8875d6b
otienojoseph <otienoj.cruz@gmail.com>
2023-09-01 23:02:44 +0300

    Fix: Put print outside if condition

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,12 +7,10 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (!n)
-		return;
-
-	if (n == 0)
-		_putchar('0');
-
-	print_binary(n >> 1);
-	_putchar(((n & 1) == 1) ? '1' : '0');
+    if (n > 1)
+	{
+		/* Equivalent to dividing the number by 2 */
+		print_binary(n >> 1);
+	}
+    _putchar('0' + (n & 1));
 }
