61e8141668e0d306d400d198feaadfb16305691e
otienojoseph <otienoj.cruz@gmail.com>
2023-09-01 23:09:37 +0300

    Fix range error

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,9 +8,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	while (n)
-	{
-		return ((n >> index) & 1);
-	}
-	return (-1);
+	if (index > 63)
+		return (-1);
+
+	return ((n >> index) & 1);
 }
