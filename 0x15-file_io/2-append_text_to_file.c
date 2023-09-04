593b28b140c9ac8097a8512f8bdd0b3fd87e68b8
OptimusJc <otienoj.cruz@gmail.com>
2023-09-05 01:41:57 +0300

    Add 2-append_text_to_file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Append text at the end of file
+ * @filename: filename
+ * @text_content: null terminated string
+ * Return: 1 on success, -1 on error
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	bytes_written = write(fd, text_content, strlen(text_content));
+	if (bytes_written == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -2,3 +2,4 @@
 
 - 0-read_textfile - Reads text file and prints it to the POSIX standard output
 - 1-create_file - Function that creates a file
+- 2-append_text_to_file - Append text content to file
