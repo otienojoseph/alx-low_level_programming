21c9f771af7e9c23ca933875e5061b3b24b52dad
otienojoseph <otienoj.cruz@gmail.com>
2023-10-21 00:18:03 +0300

    Fix spacing on output

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,88 +1,93 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <elf.h>
 #include <stdlib.h>
-
-#define BUFFER_SIZE 128
-/**
- * error_exit - Entry point
- * @message: string
- * Return: void
- */
-void error_exit(char *message)
-{
-	fprintf(stderr, "%s\n", message);
-	exit(98);
-}
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <elf.h>
 
 /**
- * print_elf_header_info - Print elf header info
- * @header: header
+ * display_elf_header - Entry point
+ * @header: header pointer
  * Return: void
  */
-void print_elf_header_info(Elf64_Ehdr *header)
+void display_elf_header(const Elf64_Ehdr *header)
 {
-	int i;
+    int i;
 
-	printf("Magic:   ");
-	for (i = 0; i < EI_NIDENT; i++)
-	{
-		printf("%02x ", header->e_ident[i]);
-	}
-	printf("\n");
-	printf("Class:							 %s\n",
-			header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-	printf("Data:							  %s\n",
-			header->e_ident[EI_DATA] == ELFDATA2LSB ?
-			"2's complement, little endian" : "2's complement, big endian");
-	printf("Version:						   %d (current)\n",
-			header->e_ident[EI_VERSION]);
-	printf("OS/ABI:							%d\n",
-			header->e_ident[EI_OSABI]);
-	printf("ABI Version:					   %d\n",
-			header->e_ident[EI_ABIVERSION]);
-	printf("Type:							  %d\n", header->e_type);
-	printf("Entry point address:			   0x%lx\n", header->e_entry);
+    printf("ELF Header:\n");
+    printf("  Magic:   ");
+    for (i = 0; i < EI_NIDENT; i++)
+    {
+        printf(" %02x", header->e_ident[i]);
+    }
+    printf("\n");
+    printf("  Class:                         %s\n",
+            (header->e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32");
+    printf("  Data:                          %s\n",
+            (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian"
+            : "2's complement, big endian");
+    printf("  Version:                       %d (current)\n",
+            header->e_ident[EI_VERSION]);
+    printf("  OS/ABI:                        %d\n", header->e_ident[EI_OSABI]);
+    printf("  ABI Version:                   %d\n", header->e_ident[EI_ABIVERSION]);
+    printf("  Type:                          %d\n", header->e_type);
+    printf("  Entry point address:           %lx\n", header->e_entry);
 }
 
 /**
  * main - Entry point
  * @argc: argument count
  * @argv: argument vector
- * Return: 0 on success, 1 on failure
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	int fd;
-	Elf64_Ehdr header;
+    int fd;
+    Elf64_Ehdr header;
 
-	if (argc != 2)
-	{
-		error_exit("Usage: elf_header elf_filename");
-	}
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+        return (98);
+    }
 
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		error_exit("Error opening file");
-	}
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
+    {
+        perror("Error opening file");
+        return (98);
+    }
 
-	if (read(fd, &header, sizeof(header)) != sizeof(header))
-	{
-		error_exit("Error reading ELF header");
-	}
+    if (read(fd, &header, sizeof(header)) != sizeof(header))
+    {
+        fprintf(stderr, "Error reading ELF header\n");
+        close(fd);
+        return (98);
+    }
 
-	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
-		header.e_ident[EI_MAG1] != ELFMAG1 ||
-		header.e_ident[EI_MAG2] != ELFMAG2 ||
-	header.e_ident[EI_MAG3] != ELFMAG3)
-	{
-		error_exit("Not an ELF file");
-	}
+    if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
+    {
+        fprintf(stderr, "Not an ELF file\n");
+        close(fd);
+        return (98);
+    }
 
-	print_elf_header_info(&header);
+    if (lseek(fd, 0, SEEK_SET) < 0)
+    {
+        perror("Error seeking file");
+        close(fd);
+        return (98);
+    }
 
-	close(fd);
-	return (0);
+    if (read(fd, &header, sizeof(header)) != sizeof(header))
+    {
+        fprintf(stderr, "Error reading ELF header\n");
+        close(fd);
+        return (98);
+    }
+
+    display_elf_header(&header);
+
+    close(fd);
+    return (0);
 }
