f951d8745c47c6e3ebe347736bd103266b289ec4
otienojoseph <otienoj.cruz@gmail.com>
2024-03-20 14:25:22 +0300

    fix: Add check if current is null

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,7 +38,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			count++;
 		}
 
-		if (current != NULL && count == idx)
+		if (current == NULL && count == idx)
+		{
+			return (NULL);
+		}
+		else if (current != NULL && count == idx)
 		{
 			current->prev->next = new;
 			new->prev = current->prev;
