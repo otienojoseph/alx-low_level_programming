e3ff1e67f59036d616c159bb0f3e61ab2e027c70
otienojoseph <otienoj.cruz@gmail.com>
2024-03-20 13:40:38 +0300

    fix: Add check for null pointer and idx is 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,12 +12,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current, *new;
 	unsigned int count;
 
-	if (*h == NULL)
-		return (NULL);
-
-	current = *h;
-	count = 0;
-
+	/* create new node */
 	new = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
@@ -25,6 +20,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	new->n = n;
+
+	if (*h == NULL)
+	{
+		if (idx == 0)
+			*h = new;
+	}
+	else if (idx == 0)
+	{
+		new->next = *h;
+		(*h)->prev = new;
+		*h = new;
+	}
+	else
+	{
+		current = *h;
+		count = 0;
+	}
 
 	while (current != NULL && count < idx)
 	{
