dffd0eb54c4aec8809e4017ad99418657aaf5b45
otienojoseph <otienoj.cruz@gmail.com>
2024-03-20 21:08:52 +0300

    fix: Add check for out of range index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -32,7 +32,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		position++;
 	}
 
-	if (current == NULL && position == index)
+	if (current == NULL && index > position)
 		return (-1);
 
 	temp = current;
